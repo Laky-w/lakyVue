dc84d414f5cf568ceae7a8bb91e282ac6c0cd02c
Laky-w <962395256@qq.com>
2017-12-19 00:56:21 +0800

    财务资金账户

--- src/components/common/Log.vue
+++ src/components/common/Log.vue
@@ -1,15 +1,15 @@
 <template>
     <div style="height:800px">
         <p class="title">项目开发重要节点记录</p>
-        <el-steps direction="vertical" :active="7" align-center>
+        <el-steps direction="vertical" :active="8" align-center>
             <el-step title="2017/11/13" description="项目开始————by laky-w"></el-step>
             <el-step title="2017/12/08" description="系统基本功能ok"></el-step>
             <el-step title="2017/12/10" description="教务模块开始启动"></el-step>
             <el-step title="2017/12/11" description="教务模块-课程功能"></el-step>
             <el-step title="2017/12/15" description="教务模块-班级功能"></el-step>
-             <el-step title="2017/12/15" description="教务模块-核心功能ok"></el-step>
+            <el-step title="2017/12/15" description="教务模块-核心功能ok"></el-step>
             <el-step title="2017/12/16" description="招生模块-启动"></el-step>
-            <el-step title="？？？" description="招生模块-生源功能"></el-step>
+            <el-step title="2017/12/17" description="招生模块基础功能完成，功能文档编写启动"></el-step>
             <el-step title="？？？" description="招生模块-跟进情况"></el-step>
             <el-step title="？？？" description="招生模块-邀约情况"></el-step>
         </el-steps>
